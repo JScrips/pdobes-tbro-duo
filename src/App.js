71f351a1f9572bbce4c4135058e8bd4ebfa2f849
Garnader <pariskdoby@gmail.com>
2023-06-07 07:07:50 -0700

    content added with multiple lists

--- src/App.js
+++ src/App.js
@@ -3,7 +3,42 @@ import './App.css';
 
 function App() {
   return (
-    <div>Hello World</div>
+  <html>
+    <head>
+      <h1>Diablo IV</h1>
+      <nav>
+        <ul>
+          <li><a href="#">Classes</a></li>
+          <li><a href="#">Maps</a></li>
+          <li><a href="#">Maps</a></li>
+        </ul>
+      </nav>
+    </head>
+    <body>
+      <h2>Diablo IV classes</h2>
+      <h3>There are 5 classes in Diablo IV</h3>
+        <ul>
+          <li><a href="#">Berserker</a></li>
+          <li><a href="#">Necromancer</a></li>
+          <li><a href="#">Sorcerer</a></li>
+          <li><a href="#">Rogue</a></li>
+          <li><a href="#">Druid</a></li>
+        </ul>
+    <div>
+      <h2>Diablo IV Maps</h2>
+      <h3>There are 5 maps in Diablo IV</h3>
+        <ul>
+          <li><a href="#">Fractured Peaks</a></li>
+          <li><a href="#">Dry Steppes</a></li>
+          <li><a href="#">Hawezar</a></li>
+          <li><a href="#">Kehjistan</a></li>
+          <li><a href="#">Scosglen</a></li>
+        </ul>
+    </div>
+    </body>
+    
+    <div>Hello Hell</div>
+    </html>
   );
 }
 
